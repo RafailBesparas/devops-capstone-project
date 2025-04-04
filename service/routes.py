749b7f87f67474bede7c8bcb4017557f038c4b50
RafaelBesparas <rafaelbesparas@outlook.com>
2025-04-04 11:45:36 -0400

    Those nose tests run successfully

--- service/routes.py
+++ service/routes.py
@@ -69,7 +69,7 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to READ an account ...
-@app.route("/accoutns/<int:account_id>", methods = ["GET"])
+@app.route("/accounts/<int:account_id>", methods = ["GET"])
 def get_accounts(account_id):
     """ 
     Reads an Account
@@ -78,7 +78,7 @@ def get_accounts(account_id):
     """
     app.logger.info("Request to read an Account with id : %s", account_id)
 
-    account = ACCOUNT.find(account_id)
+    account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
